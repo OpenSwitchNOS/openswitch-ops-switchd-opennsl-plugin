b9aba752dd27b0a2c7ba0bec51ddee47788b8e32
Jin Chong <jin.chu.chong@hpe.com>
2016-03-29 15:52:14 -0700

    PD side support for logical switch
    Change-Id: I70b75f5e2563f57683c2588385a0e82a47074644
    Signed-off-by: Jin Chong <jin.chu.chong@hpe.com>

---
+++ include/bcm-common.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2016 Hewlett-Packard Development Company, L.P.
+ * All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License"); you may
+ *   not use this file except in compliance with the License. You may obtain
+ *   a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *   License for the specific language governing permissions and limitations
+ *   under the License.
+ *
+ * File: bcm_common.h
+ *
+ * Purpose: This file provides BCM common header.
+ */
+
+#ifndef __BCM_COMMON_H__
+#define __BCM_COMMON_H__ 1
+
+
+/* Error return codes */
+#define BCMSDK_E_NONE         OPENNSL_E_NONE
+#define BCMSDK_E_INTERNAL     OPENNSL_E_INTERNAL
+#define BCMSDK_E_MEMORY       OPENNSL_E_MEMORY
+#define BCMSDK_E_UNIT         OPENNSL_E_UNIT
+#define BCMSDK_E_PARAM        OPENNSL_E_PARAM
+#define BCMSDK_E_EMPTY        OPENNSL_E_EMPTY
+#define BCMSDK_E_FULL         OPENNSL_E_FULL
+#define BCMSDK_E_NOT_FOUND    OPENNSL_E_NOT_FOUND
+#define BCMSDK_E_EXISTS       OPENNSL_E_EXISTS
+#define BCMSDK_E_TIMEOUT      OPENNSL_E_TIMEOUT
+#define BCMSDK_E_BUSY         OPENNSL_E_BUSY
+#define BCMSDK_E_FAIL         OPENNSL_E_FAIL
+#define BCMSDK_E_DISABLED     OPENNSL_E_DISABLED
+#define BCMSDK_E_BADID        OPENNSL_E_BADID
+#define BCMSDK_E_RESOURCE     OPENNSL_E_RESOURCE
+#define BCMSDK_E_CONFIG       OPENNSL_E_CONFIG
+#define BCMSDK_E_UNAVAIL      OPENNSL_E_UNAVAIL
+#define BCMSDK_E_INIT         OPENNSL_E_INIT
+#define BCMSDK_E_PORT         OPENNSL_E_PORT
+
+
+
+#endif /* __BCM_COMMON_H__ */
--- src/ops-bcm-init.c
+++ src/ops-bcm-init.c
@@ -32,6 +32,7 @@
 #include "ops-port.h"
 #include "ops-routing.h"
 #include "ops-vlan.h"
+#include "ops-vxlan.h"
 #include "ops-debug.h"
 
 VLOG_DEFINE_THIS_MODULE(ops_bcm_init);
@@ -104,6 +105,12 @@ ops_bcm_appl_init(void)
         rc = ops_l3_init(unit);
         if (rc) {
             VLOG_ERR("L3 subsystem init failed");
+            return 1;
+        }
+
+        rc = ops_vxlan_init(unit);
+        if (rc) {
+            VLOG_ERR("VXLAN subsystem init failed");
             return 1;
         }
     }
