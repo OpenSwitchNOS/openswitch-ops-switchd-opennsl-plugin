ba895860f200855fbf2a44a91ff699b4973a84dd
Jin Chong <jin.chu.chong@hpe.com>
2016-03-30 11:19:05 -0700

    PD side support of logical switch
    Change-Id: I38d4f450851fd0dbdebb306542693da58dcecf9a
    Signed-off-by: Jin Chong <jin.chu.chong@hpe.com>

--- src/ofproto-bcm-provider.c
+++ src/ofproto-bcm-provider.c
@@ -34,11 +34,13 @@
 #include "ops-vlan.h"
 #include "ops-lag.h"
 #include "ops-routing.h"
+#include "ops-vxlan.h"
 #include "ops-knet.h"
 #include "ops-mac-learning.h"
 #include "netdev-bcmsdk.h"
 #include "platform-defines.h"
 #include "ofproto-bcm-provider.h"
+#include "bcm-common.h"
 
 VLOG_DEFINE_THIS_MODULE(ofproto_bcm_provider);
 
@@ -2061,6 +2063,42 @@ get_mac_learning_hmap(struct ofproto_mlearn_hmap **mhmap)
     return ops_mac_learning_get_hmap(mhmap);
 }
 
+/* Set logical switch */
+int
+set_logical_switch(const struct ofproto *ofproto_,  void *aux,
+                   enum ofproto_logical_switch_action action,
+                   struct ofproto_logical_switch *log_switch)
+{
+    int rc = BCMSDK_E_NONE;
+    int hw_unit = 0;
+    bcmsdk_vxlan_opcode_t opcode;
+    bcmsdk_vxlan_logical_switch_t lsw;
+
+    switch (action) {
+    case OFPROTO_LOG_SWITCH_ACTION_ADD:
+        opcode = BCMSDK_VXLAN_OPCODE_CREATE;
+
+        lsw.vnid = log_switch->tunnel_key;
+
+        break;
+    case OFPROTO_LOG_SWITCH_ACTION_DEL:
+        lsw.vnid = log_switch->tunnel_key;
+        opcode = BCMSDK_VXLAN_OPCODE_DESTROY;
+        break;
+    case OFPROTO_LOG_SWITCH_ACTION_MOD:
+    default:
+        VLOG_ERR("Error [%s, %d] action:%d name:%s key:%d hw_unit:%d\n",
+                 __FUNCTION__, __LINE__,
+                 action, log_switch->name, log_switch->tunnel_key,
+                 hw_unit);
+        return 1;
+    }
+
+    rc = bcmsdk_vxlan_logical_switch_operation(hw_unit, opcode, &lsw);
+
+    return rc;
+}
+
 const struct ofproto_class ofproto_bcm_provider_class = {
     init,
     enumerate_types,
@@ -2166,4 +2204,5 @@ const struct ofproto_class ofproto_bcm_provider_class = {
     l3_ecmp_set,                /* enable/disable ECMP globally */
     l3_ecmp_hash_set,           /* enable/disable ECMP hash configs */
     get_mac_learning_hmap,      /* get mac learnt */
+    set_logical_switch,         /* set logical switch */
 };
