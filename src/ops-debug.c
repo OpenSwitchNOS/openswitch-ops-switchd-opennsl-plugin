d79a9884bfaa2e929b02ce5314310c4f1b4258de
midhan <nitish.midha@hpe.com>
2016-05-09 13:05:47 -0700

    new: dev: Plugin support for BFD.
    Change-Id: I2d71fb738445e17ac882cbca85bd6e20406f58ec
    Signed-off-by: midhan <nitish.midha@hpe.com>

--- src/ops-debug.c
+++ src/ops-debug.c
@@ -677,9 +677,9 @@ ops_fp_dump_ospf_rules (struct ds *ds)
     for(unit =0; unit < MAX_SWITCH_UNITS; unit++) {
 
         /*
-         * Get the group-id for OSPF FP ingress rules.
+         * Get the group-id for CPU Rx FP ingress rules.
          */
-        group_id = ops_routing_get_ospf_group_id_by_hw_unit(unit);
+        group_id = ops_routing_get_cpu_rx_group_id_by_hw_unit(unit);
 
         /*
          * If the group-id is invalid, then do not dump the
