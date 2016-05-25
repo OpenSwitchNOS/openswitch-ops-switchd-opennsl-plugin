2fe5c5f6a3578772f3cb9177f874b2dc74b3de76
shanmugamari <shanmugamari.s@hpe.com>
2016-05-24 23:33:05 -0700

    Merging mac changes from rel/dill to Master chg: dev: L2 MAC flush operations support
    Fix: dev: Mac learning not working for lag
    
    ops-switchd crash in mac-learning plug-in
    
    TG-1482
    
    Change-Id: I72be9ae31dcbd32dd02ac8a84e6dd1c37b8e1ec7
    Signed-off-by: shanmugamari <shanmugamari.s@hpe.com>

--- src/ops-debug.c
+++ src/ops-debug.c
@@ -51,6 +51,8 @@
 #include "ops-sflow.h"
 #include "ops-classifier.h"
 #include "netdev-bcmsdk.h"
+#include "mac-learning-plugin.h"
+#include "ops-mac-learning.h"
 
 VLOG_DEFINE_THIS_MODULE(ops_debug);
 
@@ -1771,13 +1773,18 @@ no_filter(opennsl_l2_addr_t *result __attribute__((unused)),
 static int
 process_mac_table_cb(int unit, opennsl_l2_addr_t *addr, void *ptr)
 {
+    char port_name[PORT_NAME_SIZE];
     l2_traverse_data_t *user_data = (l2_traverse_data_t *)ptr;
-    struct ops_port_info *p_info = PORT_INFO(unit, addr->port);
 
-    if (p_info == NULL || p_info->name == NULL) {
+    memset((void*)port_name, 0, sizeof(port_name));
+
+    ops_mac_learning_get_port_name(unit, addr->flags, addr->port,
+                                   addr->tgid, port_name);
+
+    if (!strlen(port_name)) {
         return 0;
     }
-    if (user_data->filter(addr, user_data->match, p_info->name)) {
+    if (user_data->filter(addr, user_data->match, port_name)) {
         ds_put_format(user_data->ds,
                       "%4d %02x:%02x:%02x:%02x:%02x:%02x %7s %5d %s\n",
                       addr->vid,
@@ -1789,7 +1796,7 @@ process_mac_table_cb(int unit, opennsl_l2_addr_t *addr, void *ptr)
                       addr->mac[5],
                       (addr->flags & OPENNSL_L2_STATIC) == 0 ? "DYNAMIC" : "STATIC ",
                       addr->tgid,
-                      p_info->name);
+                      port_name);
     }
 
     return 0;
